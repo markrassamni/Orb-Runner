c907254d8ae8bde8d31242c5955c913a87b2f670
Mark <markrassamni@me.com>
2017-11-27 14:39:15 -0800

    scene changing

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : Singleton<SceneController> {
+	
+	override protected void Awake(){
+		base.Awake();
+		DontDestroyOnLoad(this);
+	}
+
+	public void LoadGame(){
+		SceneManager.LoadScene("Game");
+	}
+
+	public void LoadMenu(){
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void ExitGame(){
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+}
+
