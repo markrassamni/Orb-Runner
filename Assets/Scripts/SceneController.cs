8ec3cb92479cabed2053ed6703321ac8b2099a17
Mark <markrassamni@me.com>
2017-12-09 19:55:41 -0800

    added sound controller to play audio

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,17 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneController : Singleton<SceneController> {
+public class SceneController : Singleton<SceneController>{
+
+	private string previousSceneName;
+	private readonly string[] gameSceneNames = {"Game" , "FirstLevel", "Level2"}; // TODO add names here
 	
 	protected override void Awake(){
 		base.Awake();
 		DontDestroyOnLoad(this);
 	}
 
+	void Start(){
+		SceneManager.sceneLoaded += OnSceneLoaded;
+		previousSceneName = SceneManager.GetActiveScene().name;
+	}
+	
+	private void OnSceneLoaded(Scene nextScene, LoadSceneMode mode){
+		if(gameSceneNames.Contains(previousSceneName)&& nextScene.name == "MainMenu"){
+			SoundController.Instance.PlayMenuMusic();
+		}
+		previousSceneName = nextScene.name; 
+	}
+
 	public void LoadGame(){
 		SceneManager.LoadScene("Game");
+		SoundController.Instance.PlayGameMusic();
 	}
 
 	public void LoadMenu(){
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : Singleton<SoundController> {
+
+	[SerializeField] private AudioClip menuMusic;
+	[SerializeField] private AudioClip gameMusic;
+	[SerializeField] private AudioClip collisionClip;
+	[SerializeField] private AudioClip gameOverClip;
+	[SerializeField] private AudioClip gameWonClip;
+ 	private AudioSource musicSource;
+	private AudioSource sfxSource;
+
+	protected override void Awake(){
+		base.Awake();
+		DontDestroyOnLoad(this);
+	}
+
+	void Start () {
+		musicSource = GetComponent<AudioSource>();
+		musicSource.loop = true;
+		musicSource.clip = menuMusic;
+		musicSource.Play();
+	}
+
+	public void PlayMenuMusic(){
+		musicSource.Stop();
+		musicSource.clip = menuMusic;
+		musicSource.Play();
+	}
+
+	public void PlayGameMusic(){
+		musicSource.Stop();
+		musicSource.clip = gameMusic;
+		musicSource.Play();
+	}
+
+	public void PlayGameWon(){
+		musicSource.Stop();
+		sfxSource.PlayOneShot(gameWonClip);
+	}
+
+	public void PlayGameOver(){
+		musicSource.Stop();
+		sfxSource.PlayOneShot(gameOverClip);
+	}
+
+	public void PlayCollision(){
+		sfxSource.PlayOneShot(collisionClip);
+	}
+
+	public void Pause(){
+		sfxSource.Pause();
+		musicSource.Pause();
+	}
+
+	public void UnPause(){
+		musicSource.UnPause();
+		sfxSource.UnPause();
+	}
+
+	public void SetVolume(float volume){
+		musicSource.volume = volume;
+	}
+
+	public void SetSfxSource(AudioSource source){
+		sfxSource = source;
+	}
+}
