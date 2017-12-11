b6e26706279906663c5a8911264975c4599d0204
William Merz <merzwilliam@gmail.com>
2017-12-10 18:25:57 -0800

    credit scene with placeholder text

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+
+    private float position = 0.0f;
+    private float on_update_movement = 1.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, on_update_movement, 0);
+    }
+}
